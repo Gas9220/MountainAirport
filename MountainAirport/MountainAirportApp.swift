a7a8a667e28b47376f89b3472c8212f93351dcfb
Gaspare <Gas9220@users.noreply.github.com>
2024-01-23 10:16:38 +0100

    Initial Commit

---
+++ MountainAirport/MountainAirportApp.swift
@@ -0,0 +1,17 @@
+//
+//  MountainAirportApp.swift
+//  MountainAirport
+//
+//  Created by Gaspare Monte on 23/01/24.
+//
+
+import SwiftUI
+
+@main
+struct MountainAirportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
