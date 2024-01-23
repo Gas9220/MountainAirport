f0bd73b7e9f335326fa3281199ca4846d387fd94
Gaspare <Gas9220@users.noreply.github.com>
2024-01-23 18:42:31 +0100

    feat: add simulation to API calls on searching cities

--- MountainAirport/SearchFlights/SearchFlights.swift
+++ MountainAirport/SearchFlights/SearchFlights.swift
@@ -8,10 +8,11 @@
 import SwiftUI
 
 struct SearchFlights: View {
-    var flightData: [FlightInformation]
+    @State var flightData: [FlightInformation]
     @State private var date = Date()
     @State private var directionFilter: FlightDirection = .none
     @State private var city = ""
+    @State private var runningSearch = false
 
     var matchingFlights: [FlightInformation] {
         var matchingFlights = flightData
@@ -21,13 +22,6 @@ struct SearchFlights: View {
                 $0.direction == directionFilter
             }
         }
-
-        if !city.isEmpty {
-            matchingFlights = matchingFlights.filter {
-                $0.otherAirport.lowercased().contains(city.lowercased())
-            }
-        }
-
         return matchingFlights
     }
 
@@ -42,6 +36,7 @@ struct SearchFlights: View {
             Calendar.current.isDate($0.localTime, inSameDayAs: date)
         }
     }
+
     var body: some View {
         ZStack {
             Image("background-view")
@@ -50,40 +45,69 @@ struct SearchFlights: View {
             VStack {
                 Picker(
                     selection: $directionFilter,
-                    label: Text("Flight Direction")
-                ) {
-                    Text("All").tag(FlightDirection.none)
-                    Text("Arrivals").tag(FlightDirection.arrival)
-                    Text("Departures").tag(FlightDirection.departure)
-                }
-                .background(Color.white)
-                .pickerStyle(SegmentedPickerStyle())
-
+                    label: Text("Flight Direction")) {
+                        Text("All").tag(FlightDirection.none)
+                        Text("Arrivals").tag(FlightDirection.arrival)
+                        Text("Departures").tag(FlightDirection.departure)
+                    }
+                    .background(Color.white)
+                    .pickerStyle(SegmentedPickerStyle())
                 List {
                     ForEach(flightDates, id: \.hashValue) { date in
-                        Section {
+                        Section(
+                            header: Text(longDateFormatter.string(from: date)),
+                            footer:
+                                Text(
+                                    "Matching flights " + "\(flightsForDay(date: date).count)"
+                                )
+                                .frame(maxWidth: .infinity, alignment: .trailing)
+                        ) {
                             ForEach(flightsForDay(date: date)) { flight in
                                 SearchResultRow(flight: flight)
                             }
-                        } header: {
-                            Text(longDateFormatter.string(from: date))
-                        } footer: {
-                            Text("Matching flights " + "\(flightsForDay(date: date).count)")
-                                .frame(maxWidth: .infinity, alignment: .trailing)
                         }
                     }
                 }
+                .overlay(
+                    Group {
+                        if runningSearch {
+                            VStack {
+                                Text("Searching...")
+                                ProgressView()
+                                    .progressViewStyle(CircularProgressViewStyle())
+                                    .tint(.black)
+                            }
+                            .frame(maxWidth: .infinity, maxHeight: .infinity)
+                            .background(.gray)
+                            .opacity(0.8)
+                        }
+                    }
+                )
                 .listStyle(InsetGroupedListStyle())
-
                 Spacer()
             }
-            .searchable(text: $city) {
+            .searchable(text: $city, prompt: "City Name") {
                 ForEach(FlightData.citiesContaining(city), id: \.self) { city in
-                    Text(city)
-                        .searchCompletion(city)
+                    Text(city).searchCompletion(city)
                 }
             }
-            .navigationBarTitle("Search Flights")
+            .onSubmit(of: .search) {
+                Task {
+                    runningSearch = true
+                    await flightData = FlightData.searchFlightsForCity(city)
+                    runningSearch = false
+                }
+            }
+            .onChange(of: city) { _, newText in
+                if newText.isEmpty {
+                    Task {
+                        runningSearch = true
+                        await flightData = FlightData.searchFlightsForCity(city)
+                        runningSearch = false
+                    }
+                }
+            }
+            .navigationTitle("Search Flights")
             .padding()
         }
     }
